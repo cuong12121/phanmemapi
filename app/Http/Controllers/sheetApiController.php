f331e640636379ba44bb3330f8aa7bdbb7afb1ad
cuong <cuongdao2692@gmail.com>
2024-03-05 10:25:07 +0700

    Update sheetApiController.php

--- app/Http/Controllers/sheetApiController.php
+++ app/Http/Controllers/sheetApiController.php
@@ -463,13 +463,13 @@ class sheetApiController extends Controller
     public function runQuantity()
     {
         
-        $data = DB::table('fs_quantity')->where('address', 2)->get();
+        $data = DB::table('fs_quantity')->where('address', 1)->get();
 
         foreach($data as $value){
 
             //lấy thông tin sản phẩm để update lại số lượng hàng hóa
 
-            $update = DB::table('fs_warehouses_products')->where('product_id', $value->product_id)->where('warehouses_id', 2)->update(['amount'=>$value->quantity]);
+            $update = DB::table('fs_warehouses_products')->where('product_id', $value->product_id)->where('warehouses_id', 1)->update(['amount'=>$value->quantity]);
 
         }
         echo "thành công";
