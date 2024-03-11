7bb6f78458ee03e21c10e31c1c48488ff92724bc
cuong12121 <cuongdao12121@gmail.com>
2024-03-11 11:02:05 +0700

    Create kho.blade.php

---
+++ resources/views/admin/kho.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.layout')
+<div id="main-content">
+    <div id="header">
+        <div class="header-left float-left">
+            <i id="toggle-left-menu" class="ion-android-menu"></i>
+        </div>
+        <div class="header-right float-right">
+            <i class="ion-ios-people"></i>
+        </div>
+    </div>
+
+    <div id="page-container">
+        
+        <button>lấy dữ liệu từ sheet</button>    
+
+        <br>
+
+        <button>cập nhật lại tồn </button> 
+        
+    </div>
+</div>
